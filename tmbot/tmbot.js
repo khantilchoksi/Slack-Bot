4625a8e71cb9cebb6d2496bfc73c463da7c92472
srjhawar <srjhawar@ncsu.edu>
2017-10-17 10:50:21 -0400

    Fixed the storyboard url which was not showing up earlier

--- tmbot/tmbot.js
+++ tmbot/tmbot.js
@@ -30,69 +30,70 @@ controller.hears('template',['mention', 'direct_mention','direct_message'], func
   var storyboardlink = '';
   var boardName= "Scrum";
   var list_lists = ['list1'];
-  promise1 = main.getNewStoryBoard(list_lists, boardName).then(function(results){
+  var promise1 = main.getNewStoryBoard(list_lists, boardName).then(function(results){
     
     storyboardlink = results[0];
     console.log('In here!!! '+storyboardlink);
+    console.log('In too here!!! '+storyboardlink);
+    
+    bot.reply(message,{
+      "text": "Following are templates of storyboards:",
+      "attachments": [
+          {
+              "title": storyboardlink,
+              "text": "Select one template from the dropdown: "
+          },
+      {	
+        "text": "Choose a game to play",
+              "fallback": "If you could read this message, you'd be choosing something fun to do right now.",
+              "color": "#3AA3E3",
+              "attachment_type": "default",
+              "callback_id": "template_selection",
+              "actions": [
+                  {
+                      "name": "templates_list",
+                      "text": "Select a template...",
+                      "type": "select",
+                      "options": [
+                          {
+                              "text": "Scrum Board",
+                              "value": "scrum"
+                          },
+              {
+                              "text": "Waterfall Board",
+                              "value": "waterfall"
+                          }
+              ]
+          }
+          ]
+          },
+      
+      
+          {
+              "fallback": "Would you like to add more lists?",
+              "title": "Would you like to add more lists in this template?",
+              "callback_id": "comic_1234_xyz",
+              "color": "#CBCFF1",
+              "attachment_type": "default",
+              "actions": [
+                  {
+                      "name": "recommend",
+                      "text": "Yes",
+                      "type": "button",
+                      "value": "yes"
+                  },
+                  {
+                      "name": "no",
+                      "text": "No",
+                      "type": "button",
+                      "value": "bad"
+                  }
+              ]
+          }
+      ]
   });
-
-  console.log('In too here!!! '+storyboardlink);
+  });
   
-  bot.reply(message,{
-    "text": "Following are templates of storyboards:",
-    "attachments": [
-        {
-            "title": storyboardlink,
-            "text": "Select one template from the dropdown: "
-        },
-    {	
-      "text": "Choose a game to play",
-            "fallback": "If you could read this message, you'd be choosing something fun to do right now.",
-            "color": "#3AA3E3",
-            "attachment_type": "default",
-            "callback_id": "template_selection",
-            "actions": [
-                {
-                    "name": "templates_list",
-                    "text": "Select a template...",
-                    "type": "select",
-                    "options": [
-                        {
-                            "text": "Scrum Board",
-                            "value": "scrum"
-                        },
-            {
-                            "text": "Waterfall Board",
-                            "value": "waterfall"
-                        }
-            ]
-        }
-        ]
-        },
-    
-    
-        {
-            "fallback": "Would you like to add more lists?",
-            "title": "Would you like to add more lists in this template?",
-            "callback_id": "comic_1234_xyz",
-            "color": "#CBCFF1",
-            "attachment_type": "default",
-            "actions": [
-                {
-                    "name": "recommend",
-                    "text": "Yes",
-                    "type": "button",
-                    "value": "yes"
-                },
-                {
-                    "name": "no",
-                    "text": "No",
-                    "type": "button",
-                    "value": "bad"
-                }
-            ]
-        }
-    ]
-});
+ 
 });
 
