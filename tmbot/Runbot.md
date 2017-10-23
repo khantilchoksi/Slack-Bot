3b9120df44bd92370119cf40ff5e53581eaf2aed
khantilchoksi <khantilchoksi@gmail.com>
2017-10-23 09:52:07 -0400

    Implemented board dropdown menu

---
+++ tmbot/Runbot.md
@@ -0,0 +1,7 @@
+##Steps
+###### Bot name on slack is : taskbot  
+
+1. run ngrok by using following command: ./ngrok http 4390  
+2. Copy the forwarding url and change the interactive components request url in slack app by https://api.slack.com/apps/A7NULAY3Z/interactive-messages  
+3. Run node tmbot.js    
+4. Use taskbot_testing channel  
--- tmbot/tmbot.js
+++ tmbot/tmbot.js
@@ -1,118 +1,159 @@
-//Work on SE_Project_CSC510 Slack channel tmbot and it has been linked to slack's testing_bot_channel
-//API token for tmbot : xoxb-253260289844-0I3ZOtHXGmZVXynApxZ8AiEF
-var Promise = require("bluebird");
+// Import express and request modules
+var express = require('express');
+var request = require('request');
 var Botkit = require('botkit');
+var Promise = require("bluebird");
 var main = require('./main.js');
 var chai = require("chai");
 var expect = chai.expect;
 
-var webhook_url = "https://hooks.slack.com/services/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";
+// Store our app's ID and Secret. These we got from Step 1. 
+// For this tutorial, we'll keep your API credentials right here. But for an actual app, you'll want to  store them securely in environment variables. 
+var clientid = '242175471667.260972372135';
+var clientsecret = 'bc75f2893363d5aeb5b178c1b68c9ac1';
+
+
+const { createMessageAdapter } = require('@slack/interactive-messages');
+
+// Initialize adapter using verification token from environment variables
+const slackMessages = createMessageAdapter('42EBVvKTcHmHGuZFMOjEcxKx');
+
+// Attach action handlers by `callback_id`
+// (See: https://api.slack.com/docs/interactive-message-field-guide#attachment_fields)
+slackMessages.action('button_tutorial', (payload,bot) => {
+ // `payload` is JSON that describes an interaction with a message.
+ console.log(`The user ${payload.user.name} in team ${payload.team.domain} pressed the welcome button`);
+
+ console.log('******* PAYLOAD : ', payload);
+ // The `actions` array contains details about the specific action (button press, menu selection, etc.)
+ const action = payload.actions[0];
+ console.log(`The button had name ${action.name} and value ${action.value}`);
+
+ // You should return a JSON object which describes a message to replace the original.
+ // Note that the payload contains a copy of the original message (`payload.original_message`).
+ 
+ //const updatedMessage = acknowledgeActionFromMessage(payload.original_message, 'button_tutorial',
+ //'I\'m getting an order started for you.');
+
+ var ackText = `You have selected ${action.value}`;
+ var replacement = payload.original_message;
+ // Typically, you want to acknowledge the action and remove the interactive elements from the message
+ 
+ //replacement.text =`Welcome ${payload.user.name}`;
+ 
+ 
+ // Start an order, and when that completes, send another message to the user.
+//  bot.startOrder(payload.user.id)
+//  .then(respond)
+//  .catch(console.error);
+
+replacement.attachments[0].text = `:white_check_mark: ${ackText}`;
+delete replacement.attachments[0].actions;
+ return replacement;
+});
+
+
+slackMessages.action('template_selection_callback', (payload,bot) => {
+    // `payload` is JSON that describes an interaction with a message.
+    console.log(`The user ${payload.user.name} in team ${payload.team.domain} pressed the welcome button`);
+   
+    console.log('******* Template PAYLOAD : ', payload);
+    // The `actions` array contains details about the specific action (button press, menu selection, etc.)
+    const action = payload.actions[0];
+    
+   
+    // You should return a JSON object which describes a message to replace the original.
+    // Note that the payload contains a copy of the original message (`payload.original_message`).
+    
+    //const updatedMessage = acknowledgeActionFromMessage(payload.original_message, 'button_tutorial',
+    //'I\'m getting an order started for you.');
+    var selected_options = action.selected_options[0];
+   console.log("Selected options: ",JSON.stringify(selected_options));
+   //console.log(`The dropdown menu had name ${action.name} and value ${action.value}`);
+    var ackText = `You have selected ${selected_options.value}`;
+    var replacement = payload.original_message;
+    // Typically, you want to acknowledge the action and remove the interactive elements from the message
+    
+    //attachment.text =`Welcome ${payload.user.name}`;
+    
+    // Start an order, and when that completes, send another message to the user.
+   //  main.startOrder(payload.user.id)
+   //  .then(respond)
+   //  .catch(console.error);
+   console.log("** Attachement: "+JSON.stringify(replacement.attachments[0]));
+   replacement.attachments[0].text = `:white_check_mark:  ${ackText}`;
+   delete replacement.attachments[0].actions;
+    return replacement;
+   });
+
+
+
+// Instantiates Express and assigns our app variable to it
+var app = express();
 
 var controller = Botkit.slackbot({
     debug: true
     //include "log: false" to disable logging
     //or a "logLevel" integer from 0 to 7 to adjust logging verbosity
-  });
-  
-// connecting the bot to a stream of messages
-var bott = controller.spawn({
-    token: process.env.TMBOTSLACKTOKEN,
+  }).configureSlackApp(
+    {
+      clientId: clientid,
+      clientSecret: clientsecret,
+      scopes: ['bot'],
+    }
+  );
+controller.spawn({
+    token: "xoxb-259926960994-cv6gxdFR7woDT6VkGrvDzphp",
     // incoming_webhook: {
     //     url: my_webhook_url
     //   }
 }).startRTM()
-// controller.setupWebserver(50001,function(err,webserver) {
-  
-//     controller.createWebhookEndpoints(controller.webserver);
-  
-//   });
-// send webhooks
-bott.configureIncomingWebhook({url: webhook_url});
 
-
-// function myincomingwebhook(owner,repoName, repoObj)
-// {
-// 	var options = {
-// 		url: urlRoot + `/repos/${owner}/${repoName}`,
-// 		method: 'PATCH',
-// 		json: payload={"text": "Trying to send message via webhook and Slack web API"},
-// 		headers: {
-// 			"User-Agent": "EnableIssues",
-// 			"content-type": "application/json",
-// 			"Authorization": token
-// 		}
-// 	};
-
-// 	// Send a http request to url and specify a callback that will be called upon its return.
-// 	request(options, function (error, response, body) 
-// 	{
-// 		console.log(body);
-// 	});
-	
-// }
 
 controller.hears('task',['mention', 'direct_mention','direct_message'], function(bot,message) 
 {
   console.log(message);
-  bot.reply(message,"Wow! You want to work on Task management with me. Awesome!");
-});
+  //bot.reply(message,"Wow! You want to work on Task management with me. Awesome!");
 
-// bot.reply(message,{
-//   "text": "Khantil following are templates of storyboards:",
-//   "attachments": [
-//       {
-//           "title": storyboardlink,
-//           "text": "Select one template from the dropdown: "
-//       },
-//   {	
-//     "text": "Choose a game to play",
-//           "fallback": "If you could read this message, you'd be choosing something fun to do right now.",
-//           "color": "#3AA3E3",
-//           "attachment_type": "default",
-//           "callback_id": "template_selection",
-//           "actions": [
-//               {
-//                   "name": "templates_list",
-//                   "text": "Select a template...",
-//                   "type": "select",
-//                   "options": [
-//                       {
-//                           "text": "Scrum Board",
-//                           "value": "scrum"
-//                       },
-//           {
-//                           "text": "Waterfall Board",
-//                           "value": "waterfall"
-//                       }
-//           ]
-//       }
-//       ]
-//       },
-  
-  
-//       {
-//           "callback": "Would you like to add more lists?",
-//           "title": "Would you like to add more lists in this template?",
-//           "callback_id": "comic_1234_xyz",
-//           "color": "#CBCFF1",
-//           "attachment_type": "default",
-//           "actions": [
-//               {
-//                   "name": "recommend",
-//                   "text": "Yes",
-//                   "type": "button",
-//                   "value": "yes"
-//               },
-//               {
-//                   "name": "no",
-//                   "text": "No",
-//                   "type": "button",
-//                   "value": "bad"
-//               }
-//           ]
-//       }
-//   ]
-// });
+  var mg = {
+    "text": "This is your first interactive message",
+    "attachments": [
+        {
+            "text": "Building buttons is easy right?",
+            "fallback": "Shame... buttons aren't supported in this land",
+            "callback_id": "button_tutorial",
+            "color": "#3AA3E3",
+            "attachment_type": "default",
+            "actions": [
+                {
+                    "name": "yes",
+                    "text": "yes",
+                    "type": "button",
+                    "value": "yes"
+                },
+                {
+                    "name": "no",
+                    "text": "no",
+                    "type": "button",
+                    "value": "no"
+                },
+                {
+                    "name": "maybe",
+                    "text": "maybe",
+                    "type": "button",
+                    "value": "maybe",
+                    "style": "danger"
+                }
+            ]
+        }
+    ]
+};
+
+bot.reply(message,mg);
+//bot.app.send(mg);
+
+//sendMessageToSlackResponseURL(responseURL, message);
+});
 
 controller.hears('template',['mention', 'direct_mention','direct_message'], function(bot,message) 
 {
@@ -128,80 +169,69 @@ controller.hears('template',['mention', 'direct_mention','direct_message'], func
     console.log('In here!!! '+storyboardlink);
     
     bot.reply(message,{
-      "text": "Khantil following are templates of storyboards:",
-      "attachments": [
-          {
-              "title": storyboardlink,
-              "text": "Select one template from the dropdown: "
-          },
-
-      
+      "text": "Hey, there!",
+      "attachments": [   
       
           {
-              "callback": "Would you like to add more lists?",
-              "title": "Would you like to add more lists in this template?",
-              "callback_id": "btn_callback",
-              "color": "#CBCFF1",
+            "text": "Choose a board template from the following list",
+            "fallback": "If you could read this message, you'd be choosing something fun to do right now.",
+              "callback_id": "template_selection_callback",
+              "color": "#3AA3E3",
               "attachment_type": "default",
               "actions": [
-                  {
-                      "name": "recommend",
-                      "text": "Yes",
-                      "type": "button",
-                      "value": "yes"
-                  },
-                  {
-                      "name": "no",
-                      "text": "No",
-                      "type": "button",
-                      "value": "bad"
-                  }
-              ]
-          }
-      ]
+              {
+                  "name": "templates_list",
+                  "text": "Select a template...",
+                  "type": "select",
+                  "options": [
+                      {
+                          "text": "Scrum Board",
+                          "value": "scrum"
+                      },
+                      {
+                          "text": "Waterfall Board",
+                          "value": "waterfall"
+                      }
+                 ]
+             }
+            ]
+        }
+    ]
   });
 
 
   });
 });
 
+// Helper functions
 
-//receive an interactive message, and reply with a message that will replace the original
-controller.on('interactive_message_callback', ['mention', 'direct_mention','direct_message'], function(bot, message) {
-        console.log("interactive messgae callback");
-        // check message.actions and message.callback_id to see what action to take...
-    
-        bot.replyInteractive(message, {
-            text: '...',
-            attachments: [
-                {
-                    title: 'My buttons',
-                    callback_id: 'btn_callback',
-                    attachment_type: 'default',
-                    actions: [
-                        {
-                            "name":"yes",
-                            "text": "Yes!",
-                            "value": "yes",
-                            "type": "button",
-                        },
-                        {
-                           "text": "No!",
-                            "name": "no",
-                            "value": "delete",
-                            "style": "danger",
-                            "type": "button",
-                            "confirm": {
-                              "title": "Are you sure?",
-                              "text": "This will do something!",
-                              "ok_text": "Yes",
-                              "dismiss_text": "No"
-                            }
-                        }
-                    ]
-                }
-            ]
-        });
-    
-    });
+function findAttachment(message, actionCallbackId) {
+    console.log("Funciton findAttachment: \n message: ", message, "/n actionCallbackID: ",actionCallbackId);
+    return message.attachments.find(a => a.callback_id === actionCallbackId);
+  }
+  
+  function acknowledgeActionFromMessage(originalMessage, actionCallbackId, ackText) {
+    console.log("Called acknowledgeActionFromMessage : \n Original Message",originalMessage);
+    console.log("actionCallbackId: ",actionCallbackId);
+    console.log("Ack Text: ",ackText);
+    const message = cloneDeep(originalMessage);
+    const attachment = findAttachment(message, actionCallbackId);
+    delete attachment.actions;
+    attachment.text = `:white_check_mark: ${ackText}`;
+    console.log("Message:: ",message);
+    return message;
+  }
+  
+  function findSelectedOption(originalMessage, actionCallbackId, selectedValue) {
+    const attachment = findAttachment(originalMessage, actionCallbackId);
+    return attachment.actions[0].options.find(o => o.value === selectedValue);
+  }  
+
+// Start the built-in HTTP server
+const port = 4390;
+slackMessages.start(port).then(() => {
+ console.log(`server listening on port ${port}`);
+});
+
+
 
