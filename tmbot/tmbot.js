403370aad4aba5a828bb42b6a00f8da9d5dc0fe5
khantilchoksi <khantilchoksi@gmail.com>
2017-11-19 20:01:15 -0500

    Added Slack Button app for distribtuion

--- tmbot/tmbot.js
+++ tmbot/tmbot.js
@@ -817,6 +817,7 @@ slackMessages.start(port).then(() => {
  console.log(`server listening on port ${port}`);
 });
 
+
 controller.hears('set due date',['mention', 'direct_mention','direct_message'], function(bot,message)
 {
   console.log(message);
@@ -906,5 +907,32 @@ controller.hears('label',['mention', 'direct_mention','direct_message'], functio
 }
 bot.reply(message,mg);
 });
+
+// // This route handles get request to a /oauth endpoint. We'll use this endpoint for handling the logic of the Slack oAuth process behind our app.
+// app.get('/oauth', function(req, res) {
+//     // When a user authorizes an app, a code query parameter is passed on the oAuth endpoint. If that code is not there, we respond with an error message
+//     if (!req.query.code) {
+//         res.status(500);
+//         res.send({"Error": "Looks like we're not getting code."});
+//         console.log("Looks like we're not getting code.");
+//     } else {
+//         // If it's there...
+//         console.log("oatuh code: "+req.query.code);
+//         // We'll do a GET call to Slack's `oauth.access` endpoint, passing our app's client ID, client secret, and the code we just got as query parameters.
+//         request({
+//             url: 'https://slack.com/api/oauth.access', //URL to hit
+//             qs: {code: req.query.code, client_id: clientId, client_secret: clientSecret}, //Query string data
+//             method: 'GET', //Specify the method
+
+//         }, function (error, response, body) {
+//             if (error) {
+//                 console.log(error);
+//             } else {
+//                 res.json(body);
+
+//             }
+//         })
+//     }
+// });
 
 
