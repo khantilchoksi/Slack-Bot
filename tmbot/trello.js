5302dbfb0f36225b0a83e491ad8fcf3d24433298
srjhawar <srjhawar@ncsu.edu>
2017-11-19 19:49:39 -0500

    Added database trial commands and trello oauth

--- tmbot/trello.js
+++ tmbot/trello.js
@@ -3,12 +3,46 @@ var _ = require("underscore");
 var request = require("request");
 var querystring = require('querystring');
 var Trello = require("node-trello");
+var pgUrl = require('pg-database-url')
+var pg = require('pg');
 
 require('dotenv').config();
 var token = process.env.TRELLO_TOKEN;
 var urlRoot = "https://api.trello.com";
 var key = process.env.TRELLO_KEY;
 var t = new Trello(key, token);
+
+function trialDatabaseConnection() 
+{
+	
+   // Example config 
+   // This may already be available from your database configuration or environment variables 
+   var dbConfig = {
+	 host: 'localhost',
+	 port: 5432,
+	 database: 'TaskBotDB',
+	 username: 'postgres',
+	 password: 'taskbot'
+   }
+	
+   var connString = pgUrl(dbConfig)
+   var client = new pg.Client(connString);
+   client.connect();
+   
+   // client.query("CREATE TABLE IF NOT EXISTS emps(firstname varchar(64), lastname varchar(64))");
+   client.query("INSERT INTO slacktotrello(slackid, trelloid) values($1, $2)", ['swatijh', 'swati378']);
+   // client.query("INSERT INTO emps(firstname, lastname) values($1, $2)", ['Mayor', 'McCheese']);
+   
+   var query = client.query("SELECT * FROM slacktotrello");
+   query.on("row", function (row, result) {
+	   result.addRow(row);
+   });
+   query.on("end", function (result) {
+	   console.log(JSON.stringify(result.rows, null, "    "));
+	   client.end();
+   });
+}
+
 
 function createNewStoryBoard(boardName)
 {
@@ -41,7 +75,7 @@ function createNewList(new_list)
 		});
 	});
 }
-
+trialDatabaseConnection();
 function createNewCard(card_name, listId)
 {
 
